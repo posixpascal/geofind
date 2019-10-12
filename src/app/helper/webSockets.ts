f75545cbbb749a96f937630e1041d0448b94fc53
Pascal Raszyk <pascal@raszyk.de>
2019-10-12 17:41:49 +0200

    use production uri

--- src/app/helper/webSockets.ts
+++ src/app/helper/webSockets.ts
@@ -4,7 +4,7 @@ import {
 
 import * as Colyseus from "colyseus.js";
 
-export const client = new Colyseus.Client(process.env.NODE_ENV === "production" ? "wss://gameserver.geofind.io" : "ws://localhost:3888");
+export const client = new Colyseus.Client(process.env.NODE_ENV !== "production" ? "wss://gameserver.geofind.io" : "ws://localhost:3888");
 
 if (process.env.NODE_ENV !== "production") {
     (window as any).client = client;
