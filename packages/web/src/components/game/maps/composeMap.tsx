76a7ccc175d5cbd68682b72acc17c88dd40b5661
Pascal Raszyk <pascal@raszyk.de>
2019-10-15 23:01:32 +0200

    removes google map api key from source

--- packages/web/src/components/game/maps/composeMap.tsx
+++ packages/web/src/components/game/maps/composeMap.tsx
@@ -6,7 +6,7 @@ export const composeMap = ({height}) => {
     return compose(
         withProps({
             containerElement: <div style={{zIndex: 15, height}}/>,
-            googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBrYhUk7lxR-FYLg5AQIpAIEyFlxsClq2k",
+            googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.GOOGLE_MAPS_API_KEY}`,
             loadingElement: <div style={{zIndex: 15, height: `100%`}}/>,
             mapElement: <div style={{zIndex: 15, height}}/>,
         }),
