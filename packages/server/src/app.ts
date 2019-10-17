24709e546607f9e52ef4f30cbe6db659fe829c8d
Pascal Raszyk <pascal@raszyk.de>
2019-10-18 01:25:21 +0200

    adds static 404 => index.html routing

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -5,13 +5,14 @@ import cors from "cors";
 import express from "express";
 import flash from "express-flash";
 import lusca from "lusca";
+import * as fs from "fs";
 
 const app = express();
 // Express configuration
 app.set("port", process.env.GAMESERVER_PORT);
 app.use(compression());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(flash());
 app.use(cors());
 app.use(lusca.xframe("SAMEORIGIN"));
@@ -19,5 +20,20 @@ app.use(lusca.xssProtection(true));
 
 app.use("/social/", socialRoutes);
 app.use("/", express.static("./public"));
+
+app.get("*", function (req, res) {
+    if (req.accepts('html')) {
+        // Respond with html page.
+        fs.readFile(__dirname + '/../public/index.html', 'utf-8', (err, page) => {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(page);
+            res.end();
+        });
+    } else if (req.accepts('json')) {
+        res.status(404).send({error: 'Not found'});
+    } else {
+        res.status(404).type('txt').send('Not found');
+    }
+});
 
 export default app;
