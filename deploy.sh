3426b719eff27611e85748ea5274a44f87f3ca1c
posixpascal <posixpascal@googlemail.com>
2021-05-26 10:40:13 +0200

    fix(i18n): adds i18n types

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-SSH='ssh -i "raszyk-plesk.pem" ubuntu@ec2-3-127-181-35.eu-central-1.compute.amazonaws.com'
+SSH='ssh -i ~/.ssh/raszyk-plesk.pem ubuntu@ec2-3-127-181-35.eu-central-1.compute.amazonaws.com'
 TARGET=/var/www/vhosts/geofind.io/httpdocs/geofind/
-$SSH 'cd $TARGET && sudo git pull origin redesign'
-$SSH 'cd $TARGET && sudo build.sh'
+$SSH "cd $TARGET && sudo git pull origin redesign"
+$SSH "cd $TARGET && sudo bash build.sh"
