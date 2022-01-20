efd0b26d02b5fc566926d3f84fbee0bf5e0ef250
posixpascal <posixpascal@googlemail.com>
2022-01-20 12:02:00 +0100

    fix: s/googlemaps/openstreetmap/g

--- packages/api/src/geocoder.ts
+++ packages/api/src/geocoder.ts
@@ -1,14 +1,24 @@
 import NodeGeocoder from "node-geocoder";
+var HttpsAdapter = require("node-geocoder/lib/httpadapter/httpsadapter.js");
+var httpAdapter = new HttpsAdapter(null, {
+  headers: {
+    "user-agent": "geofind.io <posixpascal@googlemail.com>",
+  },
+});
 
 const geocoder = NodeGeocoder({
-  provider: "google",
-  apiKey: process.env.GOOGLE_MAPS_API_KEY,
+  provider: "openstreetmap",
+  //apiKey: process.env.GOOGLE_MAPS_API_KEY,
+  email: "posixpascal@googlemail.com",
+  language: "de",
+  httpAdapter,
 });
 
 export const reverseCoordinates = async (coordinates: any) => {
   const lookupCountries = await geocoder.reverse({
     lat: coordinates[0],
     lon: coordinates[1],
+    zoom: 10,
   });
 
   return lookupCountries[0];
