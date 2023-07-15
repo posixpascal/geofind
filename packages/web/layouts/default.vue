d3e578ced0980cf1539e787155a0267f2171c52a
Pascal <posixpascal@googlemail.com>
2023-07-15 10:45:01 +0200

    fix: remove map from landing

--- packages/web/layouts/default.vue
+++ packages/web/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Navigation />
-    <main>
+    <main class="main">
         <div class="z-10"><Nuxt /></div>
     </main>
     <Footer />
@@ -28,7 +28,9 @@ export default class DefaultLayout extends Vue {
 </script>
 <style lang="postcss">
 @import url('https://fonts.googleapis.com/css?family=Luckiest+Guy&display=swap');
-
+.main {
+  @apply bg-gray-200 dark:bg-gray-800;
+}
 .font-lucky {
   position: relative;
   top: 3px;
